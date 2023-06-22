dbeafe091bc4e6fa3d9ecc9b0c4eea5feb444d69
Miguel Campos <128705638+mcsilva-dev@users.noreply.github.com>
2023-06-22 14:15:04 -0300

    Exercícios resolvidos

--- ex114.py
+++ ex114.py
@@ -1,12 +1,13 @@
 """Crie um código em python que teste se o site Pudim está acessivel pelo computador usado."""
 
+import requests.exceptions
 from requests import get
 
 print('\n', 'DESAFIO 114'.center(38), '\n')
 
 try:
     get('http://pudim.com.br/')
-except Exception as erro:
-    print(erro.__class__)
+except requests.exceptions.ConnectionError:
+    print('O site pudim não está acessível no momento.')
 else:
     print('\033[0;32mConsegui acessar o site do Pudim com sucesso!\033[m')
